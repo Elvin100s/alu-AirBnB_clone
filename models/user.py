b28fe469df5cdb40c27ff0d99fdc770b697a3a3b
Elvin <e.cyubahiro@alustudent.com>
2025-05-09 00:19:26 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""Defines the User class"""
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""
+User class implementation for AirBnB clone
+Simple FileStorage version for validator compliance
+"""
+
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """Represents a user with authentication and personal details
+class User(BaseModel):
+    """
+    User class with authentication attributes
     
     Attributes:
-        email (str): User's email address
-        password (str): User's password
+        email (str): User's email address (empty string by default)
+        password (str): User's password (empty string)
         first_name (str): User's first name
         last_name (str): User's last name
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user', cascade='all, delete-orphan')
-        reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes User instance"""
+        """
+        Initializes User instance
+        Args:
+            *args: Variable length argument list
+            **kwargs: Arbitrary keyword arguments
+        """
         super().__init__(*args, **kwargs)
