ae9950a62a558bb726f1e47cdf871927cbf972b9
Elvin <e.cyubahiro@alustudent.com>
2025-05-11 01:53:31 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""City class that inherits from BaseModel"""
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities")
+class City(BaseModel):
+    """City class with public attributes state_id and name"""
+    state_id = ""
+    name = ""
