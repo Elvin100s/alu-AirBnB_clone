5329ea49715eabd0b2607e614c2026a5e8ac9006
Elvin <e.cyubahiro@alustudent.com>
2025-05-11 02:01:39 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,83 +1,44 @@
 #!/usr/bin/python3
-""" """
-from models.city import City
-from models.user import User
-from tests.test_models.test_base_model import TestBaseModel
+"""Test for Place"""
+import unittest
 from models.place import Place
+from models.base_model import BaseModel
 
 
-class TestPlace(TestBaseModel):
-    """ Test for place"""
+class TestPlace(unittest.TestCase):
+    """Test cases for Place"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
+    def setUp(self):
+        self.place = Place()
 
-    def test_city_id(self):
-        """ """
-        new = self.value()
-        city = City()
-        new.city_id = city.id
-        self.assertEqual(type(new.city_id), str)
+    def test_inheritance(self):
+        """Validate inheritance with BaseModel"""
+        self.assertIsInstance(self.place, BaseModel)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        user = User()
-        new.user_id = user.id
-        self.assertEqual(type(new.user_id), str)
+    def test_attributes(self):
+        """5.0 Test all required attributes exist"""
+        required_attrs = {
+            "city_id": "",
+            "user_id": "",
+            "name": "",
+            "description": "",
+            "number_rooms": 0,
+            "number_bathrooms": 0,
+            "max_guest": 0,
+            "price_by_night": 0,
+            "latitude": 0.0,
+            "longitude": 0.0,
+            "amenity_ids": []
+        }
+        for attr, default in required_attrs.items():
+            self.assertTrue(hasattr(Place, attr))
+            self.assertEqual(getattr(self.place, attr), default)
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        new.name = "Place"
-        self.assertEqual(type(new.name), str)
+    def test_instance_creation(self):
+        """10.0 Correct output - Place: Instance creation"""
+        self.assertIsNotNone(self.place.id)
+        self.assertTrue(hasattr(self.place, "created_at"))
 
-    def test_description(self):
-        """ """
-        new = self.value()
-        new.description = ""
-        self.assertEqual(type(new.description), str)
 
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        new.number_rooms = 4
-        self.assertEqual(type(new.number_rooms), int)
-
-    def test_number_bathrooms(self):
-        """ """
-        new = self.value()
-        new.number_bathrooms = 2
-        self.assertEqual(type(new.number_bathrooms), int)
-
-    def test_max_guest(self):
-        """ """
-        new = self.value()
-        new.max_guest = 8
-        self.assertEqual(type(new.max_guest), int)
-
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        new.price_by_night = 18
-        self.assertEqual(type(new.price_by_night), int)
-
-    def test_latitude(self):
-        """ """
-        new = self.value()
-        new.latitude = -123.085222
-        self.assertEqual(type(new.latitude), float)
-
-    def test_longitude(self):
-        """ """
-        new = self.value()
-        new.longitude = -120.085222
-        self.assertEqual(type(new.longitude), float)
-
-    def test_amenity_ids(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+if __name__ == '__main__':
+    unittest.main()
